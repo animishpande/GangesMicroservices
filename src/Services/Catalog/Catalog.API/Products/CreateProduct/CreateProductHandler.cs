35b9b62194a6d51368f8def85da05b4784adab7e
Animish <animishpande@gmail.com>
2025-12-09 07:50:19 +0530

    Add Postgres Marten configuration for Catalog.API

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BuildingBlocks.CQRS;
 using Catalog.API.Models;
-using MediatR;
+
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, string Description, List<string> Category, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -25,9 +21,11 @@ namespace Catalog.API.Products.CreateProduct
             };
 
             // TODO: Save to Database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
 
             // Return result
-            return new CreateProductResult(Guid.NewGuid());
+            return new CreateProductResult(product.Id);
         }
     }
 }
